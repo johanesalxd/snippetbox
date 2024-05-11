394ebb1baf3f1d651876e180a18718c6bff2874b
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-05-11 22:09:55 +0800

    refactor code (2)

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
+func (app application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	files := []string{
@@ -29,7 +29,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+func (app application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -40,11 +40,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Display a form for creating a new snippet...")
 }
 
-func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	fmt.Fprint(w, "Save a new snippet...")
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,7 @@ type config struct {
 }
 
 type application struct {
+	config config
 	logger *slog.Logger
 }
 
@@ -24,23 +25,14 @@ func main() {
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	app := &application{
+	app := application{
+		config: cfg,
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", slog.String("addr", cfg.addr))
 
-	err := http.ListenAndServe(cfg.addr, mux)
+	err := http.ListenAndServe(cfg.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
