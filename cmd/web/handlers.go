46d5f8c8a668a8e7f56356b8931a595a23068b84
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-05-12 00:20:40 +0800

    implement db insert and get functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/johanesalxd/snippetbox/internal/models"
 )
 
 func (app application) home(w http.ResponseWriter, r *http.Request) {
@@ -37,15 +40,36 @@ func (app application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Display a form for creating a new snippet...")
 }
 
+// TODO: implement correct POST functionality
 func (app application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-	fmt.Fprint(w, "Save a new snippet...")
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
