cd8798d7e5f1539cce7686c188fffe42dc2aa2bf
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-05-11 20:01:03 +0800

    adding config struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,13 +6,21 @@ import (
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
 
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
@@ -20,8 +28,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting server on %s", *addr)
+	log.Printf("starting server on %s", cfg.addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
