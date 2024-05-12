aede96161e7c6ee71c15c835931a6684be1ca05e
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-05-12 12:40:14 +0800

    refactor code (3)

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"time"
 )
 
 func (app application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -37,4 +38,10 @@ func (app application) render(w http.ResponseWriter, r *http.Request, status int
 
 	w.WriteHeader(status)
 	buf.WriteTo(w)
+}
+
+func (app application) newTemplateData(r *http.Request) templateData {
+	return templateData{
+		CurrentYear: time.Now().Year(),
+	}
 }
